3da6e45d0736fb8cc161ef0d8921fb60ed00361a
Sayore <bkrippner@yahoo.de>
2023-01-04 00:10:47 +0100

    -

---
+++ src/routes/contact/+page.server.ts
@@ -0,0 +1,19 @@
+//import * as db from '$lib/server/database';
+import mysql from 'mysql2/promise';
+import Session
+
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load() {
+  const connection = await mysql.createConnection({
+    host: '188.68.55.98',
+    user: 'bkdeco',
+    password: 'BkdeIsCool00',
+    database: 'bkde'
+  });
+
+  
+
+  return {
+    posts: {fiddle:"sticks"}
+  };
+}
